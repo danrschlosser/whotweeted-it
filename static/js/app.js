d22c5019159fa1e6c6ac9778de7271b56450f92c
Evan Tarrh <evantarrh@gmail.com>
2014-02-08 08:17:44 -0500

    Updated JS, attempts at workable CSS/HTML

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,25 @@
 $().ready(function() {
-	$(".correct").fadeTo(200,0.5);
+	$(".option").click(function(e) {
+		e.preventDefault();
+		clickedName = $(this).data("candidate-name");
+		$(".option").forEach(function(option) {
+			if (option.data("candidate-name") === correctUser) {
+				option.addClass("correct");
+			}
+			else if (option.data("candidate-name") === clickedName)
+			{
+				option.addClass("wrong");
+			}
+			else
+			{
+				option.addClass("neutral");
+			}
+		})
+		if (clickedName === correctUser) {
+			window.location = "/continue/" + (score + 1);
+		}
+		else {
+			window.location = "/you_goofed" + (score);
+		}
+	})
 })
