a08b016d53f031de5c84a48c52dc3acb9f095021
benthehenten <ben.edelstein93@gmail.com>
2014-02-20 20:47:45 -0500

    works

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ def quiz():
 	    return render_template("quiz.html", data=q.makeQuiz(), score=0)
 	else:
 		messages = request.args['messages']
-		request.args['messages'] = None
 		data = q.makeQuiz()
 		#print data
 		return render_template("quiz.html", data=data, score=json.loads(messages)["score"])
